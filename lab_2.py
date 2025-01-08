39c57b11c0f29754dfd56eec26328e2efcc7b3b9
Анастасия <anirad652830@mail.ru>
2025-01-09 00:12:28 +0300

    функция чтения файла

--- lab_2.py
+++ lab_2.py
@@ -6,3 +6,7 @@ IPV4_REGEX = r"""\b(?:
     (25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\b""".strip()
 def find_ipv4_addresses(text: str) -> List[str]:
     return re.findall(IPV4_REGEX, text, re.VERBOSE)
+def read_file(file_path: str) -> str:
+    with open(file_path, 'r') as file:
+        return file.read()
+
