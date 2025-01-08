ab986884395517006ae341912c33e8ae059e87a0
Анастасия <anirad652830@mail.ru>
2025-01-09 00:11:13 +0300

    функция для поиска адресов в тексте

--- lab_2.py
+++ lab_2.py
@@ -4,3 +4,5 @@ from typing import List, Optional
 IPV4_REGEX = r"""\b(?:
     (25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}
     (25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\b""".strip()
+def find_ipv4_addresses(text: str) -> List[str]:
+    return re.findall(IPV4_REGEX, text, re.VERBOSE)
